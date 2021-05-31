22842f5bae332b5466fc89e7797131edd97fe6e0
Arlen Peiffer <arlenpeiffer@gmail.com>
2021-05-31 15:35:28 -0700

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,12 @@ export default {
         content: 'a place on the internet'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/manifest.webmanifest' }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
